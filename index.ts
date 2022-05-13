01194249620d3e5b564e3793fb9eb50d9aec015d
viettu2k <doanviettu2k@gmail.com>
2022-05-13 15:54:39 +0700

    catching error with type script

--- index.ts
+++ index.ts
@@ -2,12 +2,18 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then((response) => {
-  const todo = response.data;
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
 
   console.log(
     `The todo with ID: ${ID} Has a title of: ${title} Is it finished? ${finished}`
